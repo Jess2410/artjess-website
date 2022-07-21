b68e4d2a30e5e6da267991f15f6085ce9f51e04f
Jessica <jess2410@hotmail.fr>
2022-07-22 00:11:13 +0200

    Modal Message - style ok

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {formatDate} from '../../public/assets/utils/formatDate'
 import styles from './Modal.module.css'
+import Image from 'next/image'
 
 const Modal = ({onClose, message}) => {
 console.log("🚀 ~ file: Modal.jsx ~ line 4 ~ Modal ~ message", message)
@@ -8,12 +9,23 @@ console.log("🚀 ~ file: Modal.jsx ~ line 4 ~ Modal ~ message", message)
     return (
         <div className={styles.modalBg}>
             <div className={styles.modal}>
-                <div className="modal-content">
-                    <button className="modalbtn" onClick={onClose}>&times;</button>
-                    <p className={styles.modalP}> De : {message?.firstname} {message?.lastname}</p>
-                    <p className={styles.modalP}>{message?.email}</p>
-                    <p className={styles.modalP}>Reçu le {formatDate(message?.createdAt)}</p>
-                    <p className={styles.modalP}>{message?.message}</p>
+                <div className={styles.modalContent}>
+                    <div className={styles.modalBtnContent}>
+                    <button className={styles.modalBtn} onClick={onClose}>
+                    <Image src="/images/close.svg" alt="icon" width={30} height={30} />
+                    </button>
+                    </div>
+                    <div className={styles.modalRow}>
+                    <div className={styles.rowLeftCenter}>
+                    <Image src="/images/customers.svg" alt="icon" width={30} height={30} />
+                    <p className={styles.modalP}> 
+                    {message?.firstname} {message?.lastname} ({message?.email})</p>
+                    </div>
+                    <p className={styles.modalDate}>{formatDate(message?.createdAt)}</p>
+                    </div>
+                    <div>
+                    <p className={styles.modalText}>{message?.message}</p>
+                    </div>
                 </div>
             </div>
         </div>
