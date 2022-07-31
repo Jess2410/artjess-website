e6546aace56edd47839e229c665046c09f7d9469
Jessica <jess2410@hotmail.fr>
2022-07-31 23:51:12 +0200

    shop - cards dynamiques  à partir d'API

--- components/Shared/ImageBoxShop.jsx
+++ components/Shared/ImageBoxShop.jsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
 import styles from "./../../components/Shop/Shop.module.css";
 import {useRouter} from "next/router"
+import { BASE_URI } from '../../public/assets/app.config';
 
-export const ImageBoxShop = ({ img, alt, title, price, id, slug }) => {
+
+export const ImageBoxShop = ({ product }) => {
+  
+  console.log("🚀 ~ file: ImageBoxShop.jsx ~ line 15 ~ ImageBoxShop ~ image", product)
 const router = useRouter()
   return (
-    <div className="box">
-      <div className="iconBx">
-        <Image src={img} alt={alt} height="400px" width="400px" />
+    <div className={styles.box}>
+      <div className={styles.iconBx}>
+         {/* <Image src={product?.image} alt={alt} height="400px" width="400px" />  */}
+         <img width="100%" height={280} src={`${BASE_URI}/file/images-store/${product.image}`} alt={product.image} /> 
       </div>
-        <h2 className={styles.title}>{title}</h2>
-        <h3 className={styles.price}>$ {price}</h3>
-        <button className={styles.center} onClick={()=>router.push(`/products/${img.id}`)}>Voir</button>
+        <h2 className={styles.title}>{product?.name}</h2>
+        <h3 className={styles.price}>$ {product?.price}</h3>
+        <button className={styles.center} onClick={()=>router.push(`/products/${product?._id}`)}>Voir</button>
     </div>
   );
 };
--- components/Shop/Shop.jsx
+++ components/Shop/Shop.jsx
@@ -8,21 +8,103 @@ import { ImageBoxShop } from "../Shared/ImageBoxShop";
 import Link from "next/link";
 
 
+
+import { useState } from 'react';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
+import axios from "axios";
+import { BASE_URI } from '../../public/assets/app.config';
+import { toast } from "react-toastify";
+
 export default function Shop() {
-  const imagesBoxData = [
-    { id: "1", slug: "img1", alt: "img1", src: img1, price: "100", title: "title1" },
-    { id: "2", slug: "img2", alt: "img2", src: img2, price: "200", title: "title2" },
-    { id: "3", slug: "img3", alt: "img3", src: img3, price: "300", title: "title3" },
-    { id: "4", slug: "img4", alt: "img4", src: img1, price: "400", title: "title4" },
-    { id: "5", slug: "img5", alt: "img5", src: img2, price: "500", title: "title5" },
-    { id: "6", slug: "img6", alt: "img6", src: img3, price: "600", title: "title6" },
-    { id: "7", slug: "img7", alt: "img7", src: img1, price: "700", title: "title7" },
-    { id: "8", slug: "img8", alt: "img8", src: img2, price: "800", title: "title8" },
-    { id: "9", slug: "img9", alt: "img9", src: img3, price: "900", title: "title9" },
-    { id: "10", slug: "img10", alt: "img10", src: img1, price: "1000", title: "title10" },
-    { id: "11", slug: "img11", alt: "img11", src: img2, price: "1100", title: "title11" },
-    { id: "12", slug: "img12,", alt: "img12", src: img3, price: "1200", title: "title12" },
-  ];
+
+
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getProducts = useCallback(() => {
+    setLoading(true);
+    axios
+      .get(`${BASE_URI}/products`)
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
+  // const handleDeleteProduct = (id) => {
+  // console.log(id)
+  //   const loader = toast.loading("Veuillez patienter...");
+  //   axios
+  //     .put(`${BASE_URI}/products/delete/${id}`)
+  //     .then((res) => {
+  //       getProducts();
+  //       toast.update(loader, {
+  //         render: res.data?.message,
+  //         type: "success",
+  //         autoClose: 3000,
+  //         isLoading: false,
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       toast.update(loader, {
+  //         render: "Une erreur est survenue !",
+  //         type: "error",
+  //         autoClose: 3000,
+  //         isLoading: false,
+  //       });
+  //     });
+  // };
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const imagesBoxData = [
+  //   { id: "1", slug: "img1", alt: "img1", src: img1, price: "100", title: "title1" },
+  //   { id: "2", slug: "img2", alt: "img2", src: img2, price: "200", title: "title2" },
+  //   { id: "3", slug: "img3", alt: "img3", src: img3, price: "300", title: "title3" },
+  //   { id: "4", slug: "img4", alt: "img4", src: img1, price: "400", title: "title4" },
+  //   { id: "5", slug: "img5", alt: "img5", src: img2, price: "500", title: "title5" },
+  //   { id: "6", slug: "img6", alt: "img6", src: img3, price: "600", title: "title6" },
+  //   { id: "7", slug: "img7", alt: "img7", src: img1, price: "700", title: "title7" },
+  //   { id: "8", slug: "img8", alt: "img8", src: img2, price: "800", title: "title8" },
+  //   { id: "9", slug: "img9", alt: "img9", src: img3, price: "900", title: "title9" },
+  //   { id: "10", slug: "img10", alt: "img10", src: img1, price: "1000", title: "title10" },
+  //   { id: "11", slug: "img11", alt: "img11", src: img2, price: "1100", title: "title11" },
+  //   { id: "12", slug: "img12,", alt: "img12", src: img3, price: "1200", title: "title12" },
+  // ];
   
   const id = "product"
   return (
@@ -30,12 +112,15 @@ export default function Shop() {
       <div className="line"></div>
     <div className={styles.shop}>
       <section className={styles.shop_content}>
-        {imagesBoxData.map((img, index) => {
-          return <ImageBoxShop key={index} img={img.src} alt={img.alt} title={img.title} price={img.price}/>;
+        
+        {products.map((product, index) => {
+        console.log("my clg",product)
+          return <ImageBoxShop key={index} product={product}/>;
         })}
       </section> 
     
     </div>
+
     </>
   );
 }
