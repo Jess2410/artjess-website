7d6ac8d8ccfd19595f111ef4de3c15f206eb95e0
Jess <jess2410@hotmail.fr>
2022-09-20 15:34:21 +0200

    checkBox payment accepted

--- components/Payment_accepted/Payment_accepted.jsx
+++ components/Payment_accepted/Payment_accepted.jsx
@@ -1,21 +1,39 @@
 import axios from "axios";
-import React, {  useEffect, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./Payment_accepted.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { removeItemHandler, setUpdateQuantity, setUpdateQuantityMoins, totalAmount} from "../../redux/cart/actions";
-import { BASE_URI } from '../../public/assets/app.config';
-import Paypal from "../Paypal/Paypal";
-import Link from "next/link";
+import CheckBox from "react-animated-checkbox";
+import { useEffect } from "react";
+
 
 export default function Payment_accepted() {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
   return (
     <div className="global-container">
       <div className={styles.center}>
         <Image width={480}  height={300} src={'/images/bgjess.jpg'} alt="bg" />
       </div>
-        <h1 className={styles.center_txt}>Paiement accepté !</h1>
-        <p className={styles.center_txt}>Merci pour votre confiance.</p>
+      <div className={styles.flex}>
+      <CheckBox
+        checked={checked}
+        checkBoxStyle={{
+          checkedColor: "#34b93d",
+          size: 80,
+          unCheckedColor: "#DA727B"
+        }}
+        duration={700}
+        onClick={() => setChecked(true)}
+      />
+        <div> 
+          <h1 className={styles.center_txt}>Paiement accepté !</h1>
+          <p className={styles.center_txt}>Merci pour votre confiance.</p>
+        </div>
+      </div>
+
+        
     </div>
   );
 }
